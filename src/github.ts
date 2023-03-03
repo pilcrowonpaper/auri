a939f547a841131b88f004ff6def4e972b59c0af
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-03-04 01:13:27 +0900

    log

--- src/github.ts
+++ src/github.ts
@@ -99,6 +99,7 @@ export const getUser = async () => {
 	const emails = await getUserEmails();
 	const primaryEmail = emails.find((email) => email.primary);
 	if (!primaryEmail) throw new Error();
+	console.log(primaryEmail);
 	return {
 		username,
 		email: primaryEmail
