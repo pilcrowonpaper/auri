b7042874405f039df8ca3ce2de9632e3e5cc37cb
pilcrowOnPaper <pilcrowonpaper@gmail.com>
2023-06-23 10:49:11 +0900

    fix format

--- src/shared/format.ts
+++ src/shared/format.ts
@@ -1,11 +1,11 @@
 import { pnpm } from "../utils/execute.js";
-import { AURI_DEPLOY_SCRIPT } from "./constant.js";
+import { AURI_FORMAT_SCRIPT } from "./constant.js";
 import { getProjectPackageConfig } from "./project.js";
 
 export const formatRepository = () => {
 	const projectPackageConfig = getProjectPackageConfig();
 	const formatScriptDefined =
-		AURI_DEPLOY_SCRIPT in (projectPackageConfig.scripts ?? {});
+		AURI_FORMAT_SCRIPT in (projectPackageConfig.scripts ?? {});
 	if (!formatScriptDefined) return;
-	pnpm(AURI_DEPLOY_SCRIPT);
+	pnpm(AURI_FORMAT_SCRIPT);
 };
